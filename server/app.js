9c3814a5c5a8d8e0e5624a6bd12990704c15422a
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-09 00:42:44 +0500

    feat: clerk setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 import express from "express"
 import cors from "cors"
 import errorMiddleware from "./middlewares/error.js"
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js"
 
 const app = express()
 
+
 app.use(cors())
 app.use(express.json());
+app.use(clerkMiddleware())
+
+
+// api to listen to clerk webhook
+
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req,res) => {
     return res.send("Api is working")
