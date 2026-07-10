6ccdbd8eae0841802ecfbecaf348b41cbc410ab5
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-11 01:15:09 +0500

    fix: auth and error middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,14 +2,39 @@ import User from "../models/userModel.js";
 import ErrorHandler from "../utils/errorhandler.js";
 import catchAsyncError from "./catchAsyncError.js";
 
+export const requireClerkAuth = catchAsyncError(async (req, res, next) => {
+    const userId = req.auth?.userId;
 
-
-export const isAuthenticatedUser = catchAsyncError(async(req,res,next) => {
-    const {userId} = req.auth
-    if(!userId){
-        return next(new ErrorHandler("Please login to access this resource", 403))
+    if (!userId) {
+        return next(new ErrorHandler("Authentication required. Please login to continue.", 401));
     }
 
-    req.user = await User.findById(userId)
-    next()
-})
+    req.userId = userId;
+    next();
+});
+
+export const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+    const userId = req.auth?.userId;
+
+    if (!userId) {
+        return next(new ErrorHandler("Authentication required. Please login to continue.", 401));
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+        return next(new ErrorHandler("User profile not found. Please sync your Clerk account first.", 404));
+    }
+
+    req.userId = userId;
+    req.user = user;
+    next();
+});
+
+export const authorizeRoles = (...roles) => (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+        return next(new ErrorHandler("You are not authorized to access this resource.", 403));
+    }
+
+    next();
+};
--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,39 +1,49 @@
 import ErrorHandler from "../utils/errorhandler.js";
 
-const errorMiddleware = (err,req,res,next) => {
+const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error"
+    err.message = err.message || "Internal Server Error";
 
     // mongodb cast error
 
-    if(err.name === "CastError"){
-        const message = `Resource not found. Invalid: ${err.path}`
-        err = new ErrorHandler(message, 400)
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid value for ${err.path}.`;
+        err = new ErrorHandler(message, 400);
     }
 
     // mongoose duplicate key error
 
-    if(err.code === 11000){
-        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
-        err = new ErrorHandler(message, 400)
+    if (err.code === 11000) {
+        const field = Object.keys(err.keyValue || {})[0] || "field";
+        const message = `Duplicate ${field} entered.`;
+        err = new ErrorHandler(message, 409);
+    }
+
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map((error) => error.message).join(", ");
+        err = new ErrorHandler(message, 400);
     }
 
     // wrong jwt token
-    if(err.name === "JsonWebTokenError"){
-        const message = `JSON web token is invalid, Try again`
-        err = new ErrorHandler(message, 400)
+    if (err.name === "JsonWebTokenError") {
+        const message = "JSON web token is invalid. Please login again.";
+        err = new ErrorHandler(message, 401);
     }
 
     // jwt token expire
-    if(err.name === "TokenExpiredError"){
-        const message = `JSON web token is expired, Try again`
-        err = new ErrorHandler(message, 400)
+    if (err.name === "TokenExpiredError") {
+        const message = "JSON web token has expired. Please login again.";
+        err = new ErrorHandler(message, 401);
+    }
+
+    if (err.type === "entity.parse.failed" || (err instanceof SyntaxError && err.status === 400 && "body" in err)) {
+        err = new ErrorHandler("Invalid JSON payload.", 400);
     }
 
     res.status(err.statusCode).json({
         success: false,
         message: err.message
-    })
-}
+    });
+};
 
-export default errorMiddleware
+export default errorMiddleware;
