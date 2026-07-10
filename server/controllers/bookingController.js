6dfea73c3741334b5edec6fcf6847a89cd527d29
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-10 21:12:15 +0500

    feat: send booking mail feature

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -4,6 +4,7 @@ import Room from "../models/roomModel.js";
 import catchAsyncError from "../middlewares/catchAsyncError.js"
 import ErrorHandler from "../utils/errorhandler.js"
 import Booking from "../models/bookingModel.js";
+import sendEmail from "../utils/sendEmail.js";
 
 
 
@@ -65,6 +66,25 @@ export const createBooking = catchAsyncError(async (req, res, next) => {
         checkOutDate,
         totalPrice
     })
+
+    await sendEmail({
+            email: req.user.email,
+            subject: `Hotel Booking Details`,
+            html: `
+                <h2>Your Booking Details</h2>
+                <p>Dear ${req.user.username},</p>
+                <p>Thank you for your booking! Here are your details:</p>
+                <ul>
+                    <li><strong>Booking ID:</strong> ${booking._id}</li>
+                    <li><strong>Hotel Name</strong> ${roomData.hotel.name}</li>
+                    <li><strong>Location:</strong> ${roomData.hotel.address}</li>
+                    <li><strong>Date:</strong> ${booking.checkInDate.toDateString()}</li>
+                    <li><strong>Booking Amount:</strong> $ ${booking.totalPrice}/night</li>
+                </ul>
+                <p>We look forward to welcome you!</p>
+                <p>If you need to make any changes, feel free to contact us.</p>
+            `
+        })
 
     return res.status(201).json({ success: true, message: "Booking created successfully" })
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,11 +5,6 @@ export const getUserData =  catchAsyncError(async (req, res, next) => {
     const role = req.user.role
     const recentSearchCities = req.user.recentSearchCities
     res.status(200).json({success: true, role, recentSearchCities})
-    // const user = await User.findById(req.params.id)
-    // if (!user) {
-    //     next(new ErrorHandler(`User does not exist with id: ${req.params.id}`, 404))
-    // }
-    // res.status(200).json({ success: true, message: "User fetched successfully", user })
 })
 
 export const storeRecentSearchedCitites = catchAsyncError(async(req,res,next) => {
