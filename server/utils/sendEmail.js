ebf18da5bdfb0030506fd243c5c309d5ca3dec93
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-11 01:23:14 +0500

    fix: mail option

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,9 +1,9 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
-    const transporter = nodeMailer.createTransport({
-        host: process.env.SMPT_HOST,
-        port: process.env.SMPT_PORT,
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_HOST,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
@@ -14,10 +14,11 @@ const sendEmail = async (options) => {
         from: process.env.SMTP_MAIL,
         to: options.email,
         subject: options.subject,
-        text: options.message
-    }
+        // text: options.message,
+        html: options.html
+    };
 
-    await transporter.sendMail(mailOptions)
-}
+    await transporter.sendMail(mailOptions);
+};
 
-export default sendEmail
+export default sendEmail;
