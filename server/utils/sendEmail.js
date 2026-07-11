5cdbb2be751b36fb7f4103f8021169977961ee0b
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-11 22:31:05 +0500

    fix: smtp for booking create api

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,12 +4,12 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        service: process.env.SMTP_SERVICE,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
         }
     })
-
     const mailOptions = {
         from: process.env.SMTP_MAIL,
         to: options.email,
@@ -17,8 +17,7 @@ const sendEmail = async (options) => {
         // text: options.message,
         html: options.html
     };
-
-    await transporter.sendMail(mailOptions);
+    const res = await transporter.sendMail(mailOptions)
 };
 
 export default sendEmail;
