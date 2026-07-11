b00def4ece5e00a0677da978d1d501f1ff70ab5f
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-07-11 13:14:51 +0500

    fix: correct SMTP port env var in sendEmail and fix SMPT typo in config.env

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
